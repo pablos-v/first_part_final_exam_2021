82294ef3346fb6bddad328a0e634fd5930e3b961
Pablos <pablos@list.ru>
2021-12-27 20:54:20 +0300

    diagram and free code added

--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] startArray = { 1, 2, 3, 4, 5, 6 };
+
+int count = 0;
+for (int i = 0; i < startArray.Length; i++)
+{
+    if (startArray[i] % 2 == 0)
+    {
+        startArray[count++] = startArray[i];
+    }
+}
+int[] evenArray = new int[count];
+
+for (int i = 0; i < count; i++)
+{
+    evenArray[i] = startArray[i];
+}
+
+Console.Write(string.Join(' ', evenArray));
